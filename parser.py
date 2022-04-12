62a818e65b412d543f4ac31a14ad0e123bf8e50e
Yao Yao <yaoy.cs@gmail.com>
2022-04-12 09:28:07 -0700

    skeleton of plugin

---
+++ parser.py
@@ -0,0 +1,2 @@
+def load_annotations(data_folder):
+    pass
---
+++ version.py
@@ -0,0 +1,4 @@
+def get_release(self):
+    # Current release 121 (2022-03-02):
+    #   14,281 reactions with 12,385 unique compounds from 16,330 unique references
+    return "121"
